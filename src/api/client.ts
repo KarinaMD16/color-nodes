1cc52301532cbd0955c281662d1b818a8042e14f
Orlando Baltodano <luis.baltodano.espinoza@est.una.ac.cr>
2025-09-16 17:33:22 -0600

    jeje

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const colorNodesAPI = axios.create({
-  baseURL: "http://26.166.216.244:5197/api", // ajusta al puerto de tu backend
+  baseURL: "http://26.233.244.31:5197/api",// ajusta al puerto de tu backend
   headers: {
     "Content-Type": "application/json",
   },
